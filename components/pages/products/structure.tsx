8c85b40b45c48853e882ea0a8c30a86a737047fa
Shahzaib Fayyaz <shahzaibdev797@gmail.com>
2026-01-20 15:16:13 +0500

    NV series completed

--- components/pages/products/structure.tsx
+++ components/pages/products/structure.tsx
@@ -1,3 +1,5 @@
+import { useProducts } from "@/context/productContext";
+import { useParams } from "next/navigation";
 import React from "react";
 
 const StructureSection = () => {
@@ -8,6 +10,19 @@ const StructureSection = () => {
 		"High quality linear ways guide and high pitch ball-screw makes the difference during milling.",
 		"Super-fast cut and complex applications.",
 	];
+	const params = useParams();
+	const id = params.id;
+	const modelParam = params.model;
+
+	const { products } = useProducts();
+	const parentProduct = products.find((p: any) => p.id === Number(id));
+
+	const detailedModel = parentProduct?.models?.find(
+		(m: any) => m.name === modelParam,
+	);
+
+	const data = detailedModel.feature2.data;
+	const imageURL = detailedModel.feature2.image;
 
 	return (
 		<section className="w-full px-[5%] mx-auto py-16 md:py-24 lg:py-32 font-sans text-white">
@@ -15,7 +30,7 @@ const StructureSection = () => {
 				<div className="w-full order-1 lg:order-1">
 					<div className="relative w-full max-w-2xl mx-auto overflow-hidden rounded-2xl shadow-2xl">
 						<img
-							src="/structure.jpeg"
+							src={imageURL}
 							alt="Green Machine Technical Infographic"
 							className="w-full h-auto object-cover transform hover:scale-105 transition-transform duration-500"
 						/>
@@ -28,7 +43,7 @@ const StructureSection = () => {
 					</h2>
 
 					<ul className="space-y-4 md:space-y-5">
-						{points.map((point, index) => (
+						{data.map((point: string, index: number) => (
 							<li key={index} className="flex items-start gap-3 md:gap-4 group">
 								<span className="text-[10px] md:text-xs mt-2 shrink-0 opacity-70 group-hover:opacity-100 transition-opacity">
 									▲
