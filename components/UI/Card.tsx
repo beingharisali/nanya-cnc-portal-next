cec527c23ac0b4795d2b1ac4c4f83fb47a7f606a
Shahzaib Fayyaz <shahzaibdev797@gmail.com>
2026-01-28 16:32:12 +0500

    issues resolved

--- components/UI/Card.tsx
+++ components/UI/Card.tsx
@@ -1,5 +1,4 @@
 import { Zap, Settings, Disc, ShieldCheck, LucideIcon } from "lucide-react";
-
 import React from "react";
 import Product, { ProductSpec } from "@/types/product";
 import Link from "next/link";
@@ -7,29 +6,31 @@ import Link from "next/link";
 const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
 	return (
 		<Link href={`/products/${product.id}/product`}>
-			<div className="bg-slate-800 rounded-2xl overflow-hidden group hover:shadow-2xl hover:shadow-orange-500/20 transition-all duration-300 border border-slate-700/50">
-				<div className="relative h-68 overflow-hidden bg-gray-100">
+			<div className="relative isolate overflow-hidden bg-gray-100 rounded-2xl group hover:shadow-2xl hover:shadow-orange-500/20 transition-all duration-300 border border-slate-700/50 w-full mx-auto lg:max-w-xl xl:max-w-2xl">
+				<div className="relative h-68 md:h-80 lg:h-125 w-full bg-gray-100 flex items-center justify-center flex-col ">
 					<img
 						src={product.image}
 						alt={product.model}
-						className="w-full h-full object-fit transform transition-transform duration-500 group-hover:scale-110 "
+						className="max-w-full max-h-full object-contain transform transition-transform duration-500 group-hover:scale-105 will-change-transform"
 					/>
-					<div>
+					<div className="z-10 pointer-events-none">
 						<div className="absolute top-4 left-4 bg-orange-500 text-white text-xs font-bold px-3 py-1.5 rounded-full uppercase tracking-wider shadow-lg">
 							{product.badge}
 						</div>
-
 						<div className="absolute top-4 right-4 bg-white text-green-700 text-xs font-bold px-3 py-1.5 rounded-full uppercase tracking-wider shadow-lg">
 							<h1 className="font-semibold">{product.badge2}</h1>
 						</div>
 					</div>
+					<div className="hidden lg:flex absolute bottom-2 inset-x-0 justify-center p-5 text-center text-2xl font-extrabold transition-opacity duration-300 group-hover:opacity-0 pointer-events-none">
+						<h1 className="text-gray-800">{product.model}</h1>
+					</div>
 				</div>
 
-				<div className="p-6">
+				<div className="p-6 transition-all duration-500 ease-in-out lg:absolute lg:inset-x-0 lg:bottom-0 lg:translate-y-full lg:group-hover:translate-y-0 bg-gray-100 lg:bg-gray-800 lg:opacity-70 lg:backdrop-blur-sm lg:max-h-full lg:overflow-y-auto">
 					<h3 className="text-orange-400 text-sm font-bold mb-1">
 						{product.title}
 					</h3>
-					<h4 className="text-2xl font-bold text-white mb-6">
+					<h4 className="text-2xl font-bold lg:text-white text-gray-800 mb-6">
 						{product.model}
 					</h4>
 
@@ -40,10 +41,10 @@ const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
 									<spec.icon size={18} strokeWidth={2} />
 								</div>
 								<div>
-									<div className="text-xs text-slate-400 font-medium">
+									<div className="text-xs lg:text-slate-400 text-gray-800 font-medium">
 										{spec.label}
 									</div>
-									<div className="text-sm text-slate-200 font-bold">
+									<div className="text-sm text-gray-800 lg:text-slate-200 font-bold">
 										{spec.value}
 									</div>
 								</div>
