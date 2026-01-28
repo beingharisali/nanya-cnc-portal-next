339f5e468fcd2846d5b371443e998c5c4e29e001
Shahzaib Fayyaz <shahzaibdev797@gmail.com>
2026-01-28 17:12:01 +0500

    height decreased

--- components/UI/Card.tsx
+++ components/UI/Card.tsx
@@ -7,7 +7,7 @@ const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
 	return (
 		<Link href={`/products/${product.id}/product`}>
 			<div className="relative isolate overflow-hidden bg-gray-100 rounded-2xl group hover:shadow-2xl hover:shadow-orange-500/20 transition-all duration-300 border border-slate-700/50 w-full mx-auto lg:max-w-xl xl:max-w-2xl">
-				<div className="relative h-68 md:h-80 lg:h-120 w-full bg-gray-100 flex items-center justify-center flex-col ">
+				<div className="relative h-68 md:h-80 lg:h-105 w-full bg-gray-100 flex items-center justify-center flex-col ">
 					<img
 						src={product.image}
 						alt={product.model}
@@ -21,9 +21,9 @@ const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
 							<h1 className="font-semibold">{product.badge2}</h1>
 						</div>
 					</div>
-					<div className="hidden lg:flex absolute bottom-2 inset-x-0 justify-center p-5 text-center text-2xl font-extrabold transition-opacity duration-300 group-hover:opacity-0 pointer-events-none">
+					{/* <div className="hidden lg:flex absolute bottom-2 inset-x-0 justify-center p-5 text-center text-2xl font-extrabold transition-opacity duration-300 group-hover:opacity-0 pointer-events-none">
 						<h1 className="text-gray-800">{product.model}</h1>
-					</div>
+					</div> */}
 				</div>
 
 				<div className="p-6 transition-all duration-500 ease-in-out lg:absolute lg:inset-x-0 lg:bottom-0 lg:translate-y-full lg:group-hover:translate-y-0 bg-gray-100 lg:bg-gray-800 lg:opacity-70 lg:backdrop-blur-sm lg:max-h-full lg:overflow-y-auto">
